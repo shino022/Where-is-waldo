b151b286da5e7c61f06f2c51b76da0585e40100b
sss <sshoony95@gmail.com>
2022-08-28 19:58:20 -0400

    Add nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import logo from '../images/wally-logo.png';
+import odlaw from '../images/odlaw.png';
+import wally from '../images/wally.png';
+import whiteBeard from '../images/white-beard.png';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
@@ -10,8 +13,9 @@ const Title = styled.h1`
 
 const NavBar = styled.nav`
   padding: 1em;
-  background: papayawhip;
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const ImageWrapper = styled.div`
@@ -19,6 +23,10 @@ const ImageWrapper = styled.div`
 const Image = styled.img`
   width: 8em;
 `;
+
+const CharImage = styled.img`
+  width: 5em;
+`
 
 const NavBarLink = styled(Link)`
   font-size: 2.4rem;  
@@ -33,6 +41,9 @@ const Main = () => {
         <ImageWrapper>
           <Image src={logo} alt="Logo" />
         </ImageWrapper>
+        <CharImage src={wally} alt="Wally" />
+        <CharImage src={odlaw} alt="Odlaw" />
+        <CharImage src={whiteBeard} alt="White beard" />
         {playGame ? 
         <NavBarLink onClick={()=>{setPlayGame(false)}} to="leader">Leader Board</NavBarLink> : 
         <NavBarLink onClick={()=>{setPlayGame(true)}}  to="/">Play Game</NavBarLink>}
