e81f6a4be8313aea1d795b1dc522a4af3c398b14
shs <sshoony95@gmail.com>
2022-08-29 11:40:14 -0400

    Fix bg

--- src/components/pages/GameBoard.js
+++ src/components/pages/GameBoard.js
@@ -8,15 +8,25 @@ const Board = styled.img`
 
 const GameBoard = () => {
     const handleClick = (e) => {
-      //console.log((e.clientX - e.target.offsetLeft));//find coordinate in percentage
-      //image size: target.height, target.width
-      //mouse location: e.clientX - e.target.offsetLeft, e.clientY - e.target.offsetTop
-      const xLocation = e.clientX - e.target.offsetLeft;
-      const yLocation = e.clientY - e.target.offsetTop;
-      const xCoordinate = Math.floor(xLocation / e.target.width * 100);
-      const yCoordinate = Math.floor(yLocation / e.target.height * 100);
 
-      console.log(xCoordinate, yCoordinate);
+      const calcCoordinates = () => {
+        console.log(e);
+        const xLocation = e.pageX - e.target.offsetLeft;
+        const yLocation = e.pageY - e.target.offsetTop;
+        const xCoordinate = Math.floor(xLocation / e.target.width * 100);
+        const yCoordinate = Math.floor(yLocation / e.target.height * 100);
+        return {xCoordinate, yCoordinate};
+      };
+      
+      console.log(calcCoordinates());
+
+      //convert it to the size of targeting box
+      //store it
+      //retrieve it after user clicks a char in the list
+      //get the coordinates of the char user selected
+      //see if the coordinates are in the scope
+      //mark that char
+
     };
   return (
     <Board onClick={handleClick} src={board}/>
