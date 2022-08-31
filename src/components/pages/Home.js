188feb8cfb62813ec0a3ad81fc0cddb09574aaf0
sss <sshoony95@gmail.com>
2022-08-31 09:43:54 -0400

    Add start button

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,30 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Button = styled.button`
+`;
+
+const NavBarLink = styled(Link)`
+  font-size: 2.4rem;  
+  text-decoration: none;
+  color: black;
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <ButtonContainer>
+      <Button>
+        <NavBarLink to="game">Play Game</NavBarLink>
+      </Button>
+    </ButtonContainer>
   )
 }
 
